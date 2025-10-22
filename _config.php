c2ce3fdc864d28a6fc86fc3e3f3ab7ad2f1dee8d
TonyxTony <tonyxkiller43@gmail.com>
2025-10-22 16:46:02 +0530

    Update _config.php

--- _config.php
+++ _config.php
@@ -1,19 +1,12 @@
-<?php 
-$conn = mysqli_connect("localhost", 'root' , '', "anime") or die("Connection fail");
+<?php
+// Database Configuration
+define('DB_HOST', 'sql12.freesqldatabase.com');
+define('DB_PORT', '3306');
+define('DB_USER', 'sql12804085');
+define('DB_PASS', 'VBPhaqwQTa');
+define('DB_NAME', 'sql12804085');
 
+// Site Configuration
+define('SITE_URL', 'https://your-app-name.onrender.com');
 
-$websiteTitle = "Zoro";
-$websiteUrl = "http://localhost:3000";
-$websiteLogo = $websiteUrl . "/files/images/logo_zoro.png";
-$contactEmail = "@gmail.com";
-
-$version = "0.1";
-
-$discord = "https://discord.gg/7JKJSbnHqf";
-$github = "https://github.com/shafat-96";
-$twitter = "https://x.com/racistprogrammer";
-$disqus = "https://.disqus.com/embed.js";
-$api = "{your-api}/api"; 
-
-$banner = $websiteUrl . "/files/images/banner.png";
 ?>
